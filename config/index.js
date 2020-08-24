1520664036cd16e47100453d7662e36abe80daf6
shikai.liu <shikai.liu@ly.com>
2020-08-24 08:02:36 +0800

    modify the proxy port

--- config/index.js
+++ config/index.js
@@ -32,7 +32,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                target: 'http://localhost:9394',
+                target: 'http://localhost:9393',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/': '/'
