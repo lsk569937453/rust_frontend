c1750abb15e8218a59fd80549d98d76c03df0148
Your Name <you@example.com>
2020-09-12 16:24:35 +0800

    modify the format code

--- src/utils/axiosUtils.js
+++ src/utils/axiosUtils.js
@@ -1,13 +1,12 @@
 import axios from 'axios'; // 引入axios
 
 
-
 var Request = axios.create(
-  {
+    {
 
-    timeout: 1000000,
-    headers: { 'X-Custom-Header': 'foobar', 'userAgent': navigator.userAgent }
-  }
+        timeout: 1000000,
+        headers: {'X-Custom-Header': 'foobar', 'userAgent': navigator.userAgent}
+    }
 );
 Request.all = axios.all;
 Request.spread = axios.spread;
