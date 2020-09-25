b0e06ac1babcf33604b210b01d040be2408b68c8
shikai.liu <shikai.liu@ly.com>
2020-09-25 09:37:29 +0800

    modify the http cron demo recent exec result

--- src/components/addHttpTask.vue
+++ src/components/addHttpTask.vue
@@ -48,10 +48,21 @@
         <h1>Cron Demo</h1>
       </div>
 
-      <div>
+      <div class="desCls">
         <el-table :data="tableData"
                   stripe
+                  @row-click="clickTable"
+                  ref="refTable"
                   style="width: 100%">
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <span class="demoSpan"
+                    v-for="(item,i) in  props.row.desc"
+                    :key="i">2020-08-20 05:20:20
+              </span>
+
+            </template>
+          </el-table-column>
           <el-table-column prop="cron"
                            label="cronExpression"
                            width="180">
@@ -61,16 +72,7 @@
           </el-table-column>
 
         </el-table>
-        <el-collapse v-model="activeNames" @change="handleChange">
-          <el-collapse-item :title="item.cron+'    '+item.desc" :name="i" v-for="(item,i) in tableData">
-            <div>2020-08-20 05:20:20</div>
-            <div>2020-08-20 05:20:20</div>
-            <div>2020-08-20 05:20:20</div>
-            <div>2020-08-20 05:20:20</div>
-            <div>2020-08-20 05:20:20</div>
-          </el-collapse-item>
 
-        </el-collapse>
         <!--                  v-model="form.desc"-->
 
       </div>
@@ -87,7 +89,7 @@ import expressionUtils from "../utils/cronSample.js"
 
 export default {
   name: "addTaskPage",
-  data() {
+  data () {
     return {
       activeNames: ['1'],
       tableData: [],
@@ -103,25 +105,44 @@ export default {
       },
       rules: {
         taskName: [
-          {required: true, message: "请输入活动名称", trigger: "blur"},
+          { required: true, message: "请输入活动名称", trigger: "blur" },
         ],
         cronExpressionInput: [
-          {required: true, message: "请输入cron表达式", trigger: "blur"},
+          { required: true, message: "请输入cron表达式", trigger: "blur" },
         ],
-        url: [{required: true, message: "请输入url", trigger: "blur"}],
+        url: [{ required: true, message: "请输入url", trigger: "blur" }],
       },
     };
   },
 
-  mounted() {
+  mounted () {
     // document
     //   .querySelector("body")
     //   .setAttribute("style", "background-color:#C0C4CC");
     this.tableData = expressionUtils.getAllExpression()
   },
   methods: {
+    clickTable (row, index, e) {
+      this.$refs.refTable.toggleRowExpansion(row)
+    }
+    ,
+    getTitle (cron, desc) {
+      let baseline = 30;
+      let loopIndex = baseline - cron.length;
+      let res = cron;
+      let spaceChar = "";
+      for (var i = 0; i < loopIndex; i++) {
+        spaceChar = spaceChar + "\u00A0" + "\u00A0"
+      }
+      let newres1 = res + spaceChar + desc
+      console.log(newres1)
+      return newres1
+    },
+    handleChange () {
+      console.log("a")
+    },
 
-    openFullScreen1() {
+    openFullScreen1 () {
       this.fullscreenLoading = true;
       const loading = this.$loading({
         lock: true,
@@ -138,88 +159,88 @@ export default {
       }, 2000);
     },
 
-    open1(msg) {
+    open1 (msg) {
       const h = this.$createElement;
 
       this.$notify({
         title: "error message",
-        message: h("i", {style: "color: red"}, msg),
+        message: h("i", { style: "color: red" }, msg),
       });
     },
-    handleRemove(file, fileList) {
+    handleRemove (file, fileList) {
       console.log(file, fileList);
     },
-    handlePreview(file) {
+    handlePreview (file) {
       console.log(file);
     },
-    checkCron() {
+    checkCron () {
       Request.post("/api/check/task", {
         name: this.formLabelAlign.taskName,
         cron_expression: this.formLabelAlign.cronExpressionInput,
         url: this.formLabelAlign.url,
       })
-          .then((response) => {
-            let data = response.data;
-            if (data.res_code == -1) {
-              this.open1(data.message);
-            } else if (data.res_code == 0) {
-              this.checklog = data.message;
-            }
-            console.log(response);
-            self.time = response.data;
-          })
-          .catch((response) => {
-            console.log(response);
-          });
+        .then((response) => {
+          let data = response.data;
+          if (data.res_code == -1) {
+            this.open1(data.message);
+          } else if (data.res_code == 0) {
+            this.checklog = data.message;
+          }
+          console.log(response);
+          self.time = response.data;
+        })
+        .catch((response) => {
+          console.log(response);
+        });
     },
-    addCronTask() {
+    addCronTask () {
       Request.post("/api/task/add", {
         name: this.formLabelAlign.taskName,
         cron_expression: this.formLabelAlign.cronExpressionInput,
         url: "http://" + this.formLabelAlign.url,
       })
-          .then((response) => {
-            let data = response.data;
-            if (data.resCode == -1) {
-              this.open1(data.message);
-            } else if (data.resCode == 0) {
-              this.openFullScreen1();
+        .then((response) => {
+          let data = response.data;
+          if (data.resCode == -1) {
+            this.open1(data.message);
+          } else if (data.resCode == 0) {
+            this.openFullScreen1();
 
-              //  this.checklog = data.message;
-            } else if (data.resCode == -2) {
-              let messageArray = response.data.message;
-              var message = messageArray.map(function (item) {
-                return item.message;
-              })
-              console.log(message)
-              this.$notify({
-                title: 'NOTIFY',
-                message: message
-              });
-            }
-            console.log(response);
-            self.time = response.data;
-          })
-          .catch((response) => {
-            console.log(response);
-          });
+            //  this.checklog = data.message;
+          } else if (data.resCode == -2) {
+            let messageArray = response.data.message;
+            var message = messageArray.map(function (item) {
+              return item.message;
+            })
+            console.log(message)
+            this.$notify({
+              title: 'NOTIFY',
+              message: message
+            });
+          }
+          console.log(response);
+          self.time = response.data;
+        })
+        .catch((response) => {
+          console.log(response);
+        });
     },
 
-    handleClose(done) {
+    handleClose (done) {
       this.$confirm("确认关闭？")
-          .then((_) => {
-            done();
-          })
-          .catch((_) => {
-          });
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {
+        });
     },
-    handleExceed(files, fileList) {
+    handleExceed (files, fileList) {
       this.$message.warning(
-          `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length
-          } 个文件`
+        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length
+        } 个文件`
       );
     },
-    beforeRemove(file, fileList) {
+    beforeRemove (file, fileList) {
       return this.$confirm(`确定移除 ${file.name}？`);
     },
 
@@ -235,17 +256,19 @@ export default {
 /*.formCls /deep/ .el-input {*/
 /*  width: auto !important;*/
 /*}*/
-
+.demoSpan {
+  word-break: normal;
+  width: auto;
+  display: block;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  overflow: hidden;
+}
 .formInput {
   width: 30px;
 }
 
 .firstCard {
   margin: 30px 100px 30px 100px;
-}
-
-.desCls /deep/ .el-collapse-item__content {
-  text-align: left !important;
-
 }
 </style>
