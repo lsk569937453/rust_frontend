e6bc053b3b52e4dc2ee016db4e0f7a4cee484ed4
shikai.liu <shikai.liu@ly.com>
2020-10-10 09:18:34 +0800

    modify the Base_url

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -32,7 +32,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     plugins: [
         // http://vuejs.github.io/vue-loader/en/workflow/production.html
         new webpack.DefinePlugin({
-            'process.env': env
+            'process.env': env,
+            BASE_URL: '"/static/pc/"'
+
         }),
         // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
         // new webpack.optimize.UglifyJsPlugin({
