d7ac3c7cf8b1159da2d5a288f73fb3059aff3b9d
shikai.liu <lsk569937453@gmail.com>
2020-10-20 22:54:20 +0800

    add the code of verify

--- src/components/login.vue
+++ src/components/login.vue
@@ -10,7 +10,8 @@
                :rules="rules"
                ref="login"
                label-width="0px"
-               class="ms-content">
+               class="ms-content"
+      >
         <el-form-item prop="username">
           <el-input v-model="param.username"
                     placeholder="username">
@@ -27,6 +28,21 @@
                        icon="el-icon-lock"></el-button>
           </el-input>
         </el-form-item>
+        <el-form-item class="verifyForm">
+          <el-col :span="16">
+            <!--            <el-input v-model="verifyCodeInput" maxlength="4">-->
+
+            <!--            </el-input>-->
+
+            <CodeInput :loading="false" class="input" :fields="4" :fieldHeight="40"/>
+
+          </el-col>
+          <el-col :span="4" :offset="2">
+            <div @click.stop.prevent="refreshVerifyCode">
+              <verify-code :identify-code="identifyCode"></verify-code>
+            </div>
+          </el-col>
+        </el-form-item>
         <div class="login-btn">
           <el-button type="primary"
                      @click="submitForm()">Sign In
@@ -34,17 +50,26 @@
         </div>
         <p class="login-tips">Tips : Fill in the user name and password randomly</p>
       </el-form>
+
     </el-card>
   </div>
 </template>
 
 <script>
 import Request from "../utils/axiosUtils";
+import verifyCode from "./verifyCode"
+import CodeInput from "vue-verification-code-input";
 
 export default {
   name: "login",
+  components: {
+    verifyCode,
+    CodeInput
+  },
   data() {
     return {
+      verifyCodeInput: "",
+      identifyCode: "111",
       param: {
         username: "admin",
         password: "123123",
@@ -61,6 +86,10 @@ export default {
   mounted() {
   },
   methods: {
+    refreshVerifyCode() {
+      const randomNumber = Math.floor(Math.random() * 9000) + 1000
+      this.identifyCode = randomNumber.toString();
+    },
     submitForm() {
       this.$router.push({name: "shareFile"});
     },
@@ -75,6 +104,16 @@ export default {
   height: 100%;
   background-image: url(../../static/login-bg.jpg);
   background-size: 100%;
+}
+
+.verifyForm /deep/ .react-code-input > input:first-child {
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+
+.verifyForm /deep/ .react-code-input > input:last-child {
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
 }
 
 .loginDiv {
